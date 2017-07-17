a07082e7db780159ed6c8878f27707645b6dd3c5
Zortax <leonard.seibold.m@gmail.com>
2017-07-17 15:11:26 +0200

    serialization stuff

--- Network/src/main/java/de/zortax/pra/network/serialization/impl/types/ArraySerializer.java
+++ Network/src/main/java/de/zortax/pra/network/serialization/impl/types/ArraySerializer.java
@@ -1,3 +1,23 @@
+/*
+
+    PraFramework - A simple TCP-Networking framework for Java
+    Copyright (C) 2017  Zortax
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
 package de.zortax.pra.network.serialization.impl.types;// Created by leo on 14.07.17
 
 import de.zortax.pra.network.serialization.FieldSerializer;
@@ -89,9 +109,6 @@ public class ArraySerializer implements FieldSerializer<ArrayContainer> {
             if (object instanceof Object[]) {
                 ArrayList<Byte> bytes = new ArrayList<>();
                 Object[] array = (Object[]) object;
-                //byte[] size = CharSerializer.toByteArray((char) array.length);
-                //bytes.add(size[0]);
-                //bytes.add(size[1]);
                 for (Object c : array) {
                     byte[] elementBytes = toByteArray(c, serializer);
                     if (c.getClass().isArray()
